7cfe12b91ed1ceae74bb1f34c41e7e7c779513fb
Daniel Ribeiro <asthedayislong@gmail.com>
2017-08-07 09:21:24 -0300

    rails new

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'questions_controller/answer'
+
+  get 'questions_controller/ask'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
