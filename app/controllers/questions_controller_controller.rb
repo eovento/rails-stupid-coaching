09b27504f3d65d1f0b235a700847a0d60512bd9b
Daniel Ribeiro <asthedayislong@gmail.com>
2017-08-07 12:07:28 -0300

    done

--- app/controllers/questions_controller_controller.rb
+++ app/controllers/questions_controller_controller.rb
@@ -1,5 +1,12 @@
 class QuestionsControllerController < ApplicationController
+
   def answer
+  	@query = params[:query]
+  	if @query == "I am going to work right now!"
+  		@coach_answer = "That's my boy!"
+  	else
+  		@coach_answer = "You are just a waste of my time... :/"
+  	end
   end
 
   def ask
